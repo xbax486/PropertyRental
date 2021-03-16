a15e899f2c9366d88785953a09a7d05c7f37ab41
Xiang Ba <xbax486@gmail.com>
2021-03-16 14:16:06 +1100

    Reset the available column of property records.

---
+++ Migrations/20210316030744_UpdateAvailableColumnOfAllRecordsInPropertiesTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PropertyRental.Migrations
+{
+    public partial class UpdateAvailableColumnOfAllRecordsInPropertiesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Properties SET Available = 'TRUE' WHERE Id in (11, 14, 15, 16, 23)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Properties SET Available = 'FALSE' WHERE Id in (11, 14, 15, 16, 23)");
+        }
+    }
+}
