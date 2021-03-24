4701fe2f7e911a3839e08960880391236dfe7550
Xiang Ba <xbax486@gmail.com>
2021-03-24 23:51:40 +1100

    Update the availability of unavailable properties.

---
+++ Migrations/20210324124313_UpdateAvailableForProperties.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PropertyRental.Migrations
+{
+    public partial class UpdateAvailableForProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Properties SET Available = 'TRUE' WHERE Id in (1, 2, 9)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Properties SET Available = 'FALSE' WHERE Id in (1, 2, 9)");
+        }
+    }
+}
