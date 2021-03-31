42238b0a71667221cbf72e7025cd93a65d2af5e5
Xiang Ba <xbax486@gmail.com>
2021-03-31 11:37:46 +1100

    Add migration to reset property availability.

---
+++ Migrations/20210331002617_UpdateAvailablePropertiesAfterFixingRentalUpdateBug.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PropertyRental.Migrations
+{
+    public partial class UpdateAvailablePropertiesAfterFixingRentalUpdateBug : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Properties SET Available = 'TRUE' WHERE Id in (1, 3, 4, 6, 9, 11, 15, 17)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Properties SET Available = 'FALSE' WHERE Id in (1, 3, 4, 6, 9, 11, 15, 17)");
+        }
+    }
+}
